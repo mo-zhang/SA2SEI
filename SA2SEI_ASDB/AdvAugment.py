e46bc8d16be514f5b3745be42d426800de367006
墨彰 <73431570+mo-zhang@users.noreply.github.com>
2024-04-29 17:09:20 +0800

    Update AdvAugment.py

--- SA2SEI_ASDB/AdvAugment.py
+++ SA2SEI_ASDB/AdvAugment.py
@@ -9,23 +9,27 @@ class AdvAug(nn.Module):
 
     def __init__(self, online_network, n_power=1, XI=0.01, epsilon=1.0):
         super(AdvAug, self).__init__()
+        # 初始化函数，接收网络模型、VAT中计算对抗扰动的次数、生成对抗扰动的超参数
         self.online_network = online_network
         self.n_power = n_power
         self.XI = XI
         self.epsilon = epsilon
 
     def forward(self, X):
+        # 前向传播函数，生成对抗样本增强后的样本并返回
         aug_sample = generate_adv_aug_sample(X, self.online_network, self.n_power, self.XI, self.epsilon)
         return aug_sample
 
 def get_normalized_vector(d):
+    # 计算归一化向量
     d_reshaped = d.view(d.shape[0], -1, *(1 for _ in range(d.dim() - 2)))
     d /= torch.norm(d_reshaped, dim=1, keepdim=True) + 1e-8
     return d
 
 def generate_adv_perturbation(x, online_network, n_power, XI, epsilon):
+    # 生成对抗扰动的函数
     d = torch.randn_like(x)
-    loss_mse = nn.MSELoss()
+    loss_mse = nn.MSELoss()  # 使用均方误差作为损失函数
     if torch.cuda.is_available():
         loss_mse = loss_mse.cuda()
 
@@ -35,10 +39,11 @@ def generate_adv_perturbation(x, online_network, n_power, XI, epsilon):
         feature_ad = online_network(x + d)[0]
         dist = loss_mse(feature, feature_ad)
         grad = torch.autograd.grad(dist, [d])[0]
-        d = grad.detach()
+        d = grad.detach()  # 更新扰动
     return epsilon * get_normalized_vector(d)
 
 def generate_adv_aug_sample(x, online_network, n_power, XI, epsilon):
+    # 生成对抗样本增强的函数
     r_adv = generate_adv_perturbation(x, online_network, n_power, XI, epsilon)
-    x_adv = x + r_adv
-    return x_adv.detach()
+    x_adv = x + r_adv  # 将对抗性扰动添加到输入样本上
+    return x_adv.detach()   # 返回增强后的对抗样本
